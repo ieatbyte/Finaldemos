39997920657398e7e5a82806f6faec1568b6b1a1
Hui Wang <ieatbyte@gmail.com>
2016-01-13 16:45:42 +0800

    change relative.layoutparams to viewgroup.layoutparams

--- FinalDemos/app/src/main/java/com/wh/finaldemos/demos/androidviews/popupwindow/PopupWindowDemoActivity.java
+++ FinalDemos/app/src/main/java/com/wh/finaldemos/demos/androidviews/popupwindow/PopupWindowDemoActivity.java
@@ -2,9 +2,9 @@ package com.wh.finaldemos.demos.androidviews.popupwindow;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.PopupWindow;
-import android.widget.RelativeLayout;
 
 import com.wh.finaldemos.BaseDemoActivity;
 import com.wh.finaldemos.R;
@@ -22,7 +22,7 @@ public class PopupWindowDemoActivity extends BaseDemoActivity {
 
         but1 = (Button) findViewById(R.id.but1);
         View content = getLayoutInflater().inflate(R.layout.popup_window_demo_layout, null);
-        mPopupWindow = new PopupWindow(content, RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT, false);
+        mPopupWindow = new PopupWindow(content, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, false);
         content.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
